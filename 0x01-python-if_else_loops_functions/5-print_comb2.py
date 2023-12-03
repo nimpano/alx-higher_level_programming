36389f96174c0fc746529a7cdda16ab77adc831d
nimpano <naphtarimpano@gmail.com>
2023-12-03 02:34:29 -0800

    leading zero

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(0, 100, 1):
     if number == 99:
-        print("{}".format(number))
+        print("{}".format(number), end="\n")
     else:
         print("{:02}".format(number), end=", ")
 
