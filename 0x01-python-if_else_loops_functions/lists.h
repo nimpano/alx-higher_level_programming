b5aaf5289c6681441628dc88705b77842b77bec1
nimpano <naphtarimpano@gmail.com>
2023-12-03 03:08:21 -0800

    I do not careless

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,25 @@
+#ifndef listt-h
+#define lists-h
+
+def islower(c):
+
+def uppercase(str):
+
+def print_last_digit(number):
+
+def add(a, b):
+
+
+def pow(a, b):
+
+def fizzbuzz():
+
+
+
+
+
+
+
+#endif
+
+
