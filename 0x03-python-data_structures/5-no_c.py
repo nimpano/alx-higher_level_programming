efba6706d5b01378c4a73bd536177f3f17cbe385
nimpano <naphtarimpano@gmail.com>
2023-12-06 14:43:27 -0800

    Its okay

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,13 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+
 
 #include "LIST_H"
 
 def no_c(my_string):
-	
 	new_string = ""
 	for i in my_string:
-		
 		if i == 'c' or i == 'C':
-			continue			
+			continue
 		new_string += i
 	return new_string
+
